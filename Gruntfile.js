2a7b4022d2d43ffe5fd22c1feb2798253cc29b74
Marc Qualie <marc@marcqualie.com>
2015-08-11 19:46:07 +0100

    Huge overhaul to move assets to bower and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-exec");
+
+  grunt.initConfig({
+
+    copy: {
+      fontawesome: {
+        files: [{
+          expand: true,
+          flatten: true,
+          src: '_assets/bower/fontawesome/fonts/*',
+          dest: 'assets/fonts/'
+        }]
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          sourcemap: 'none',
+          loadPath: '_assets/bower',
+          style: 'compressed',
+        },
+        files: {
+          'assets/stylesheets/application.css': '_assets/stylesheets/application.scss'
+        }
+      }
+    },
+
+    exec: {
+      jekyll_build: {
+        cmd: "jekyll build --trace"
+      }
+    },
+
+    watch: {
+      source: {
+        files: [
+          "_assets/**/*",
+          "_layouts/**/*",
+          "_articles/**/*",
+          "_data/**/*",
+          "_config.yml",
+          "*.html",
+          "*.haml",
+          "*.md"
+        ],
+        tasks: [
+          'sass',
+          'copy',
+          "exec:jekyll_build"
+        ]
+      }
+    }
+
+  });
+
+  grunt.registerTask('serve', [
+    'sass',
+    'copy',
+    'exec:jekyll_build',
+    'watch',
+  ]);
+
+};
